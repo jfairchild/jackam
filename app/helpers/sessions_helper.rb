1d8acf2825c8a3a62275fc83d7f596c6ee627665
Jeff Fairchild <jeff@crisistextline.org>
2018-08-18 15:24:27 -0400

    parameterize redirect url

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,18 +5,16 @@ module SessionsHelper
   TWITCH_AUTH_BASE = 'https://id.twitch.tv/'
   TWITCH_API_BASE = 'https://api.twitch.tv/kraken/'
   TWITCH_SCOPES = 'scope=user_read+user_subscriptions'
-  REDIRECT_URL = 'http://localhost:3000/login'
   # Jack AM
   CHANNEL_ID = '108392761'
-  GO_OFF_KINGS_ID = '169164648'
 
   def self.twitch_auth_url(state_token)
     tok = Base64.urlsafe_encode64(state_token)
-    "#{TWITCH_AUTH_BASE}oauth2/authorize?client_id=#{ENV['TWITCH_CLIENT_ID']}&redirect_uri=#{REDIRECT_URL}&response_type=code&#{TWITCH_SCOPES}&state=#{tok}"
+    "#{TWITCH_AUTH_BASE}oauth2/authorize?client_id=#{ENV['TWITCH_CLIENT_ID']}&redirect_uri=#{ENV['REDIRECT_URL']}&response_type=code&#{TWITCH_SCOPES}&state=#{tok}"
   end
 
   def self.twitch_auth_user(twitch_code)
-    "#{TWITCH_AUTH_BASE}oauth2/token?client_id=#{ENV['TWITCH_CLIENT_ID']}&client_secret=#{ENV['TWITCH_CLIENT_SECRET']}&code=#{twitch_code}&grant_type=authorization_code&redirect_uri=#{REDIRECT_URL}"
+    "#{TWITCH_AUTH_BASE}oauth2/token?client_id=#{ENV['TWITCH_CLIENT_ID']}&client_secret=#{ENV['TWITCH_CLIENT_SECRET']}&code=#{twitch_code}&grant_type=authorization_code&redirect_uri=#{ENV['REDIRECT_URL']}"
   end
 
   def self.twitch_user(twitch_user_code)
